d3a8436c676839f728948e76d0b4f3b570a23e21
Gabriel Kanatschnig <ic16b061@technikum-wien.at>
2018-06-08 23:07:04 +0200

    Update to latest changes (Model Recipe)

--- app/Recipe.php
+++ app/Recipe.php
@@ -6,7 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Recipe extends Model
 {
-    protected $table = 'recipe';
-
-
+    //
 }
--- database/migrations/2018_06_08_173852_create_recipes_table.php
+++ database/migrations/2018_06_08_173852_create_recipes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTableRecipe extends Migration
+class CreateRecipesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateTableRecipe extends Migration
      */
     public function up()
     {
-        Schema::create('recipe', function (Blueprint $table) {
+        Schema::create('recipes', function (Blueprint $table) {
             $table->increments('id');
             $table->string('title');
             $table->binary('image');
@@ -31,6 +31,6 @@ class CreateTableRecipe extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('recipe');
+        Schema::dropIfExists('recipes');
     }
 }
